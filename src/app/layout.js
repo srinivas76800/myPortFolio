84f8d413407f4af18937c9e88d3103c832f8ef3c
Vemunuri Vivek <srinivasvemunuri8@email.com>
2026-01-21 07:20:46 +0530

    forgot to add speed component

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
       >
         <ProjectComponent>
           <Stairs>{children}</Stairs>
+          <SpeedInsights />
         </ProjectComponent>
       </body>
     </html>
